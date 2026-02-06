8d4738cc9a713270f9aa0f2159cc1ec65dc8bd9c
Libin C Jacob <libinn11@gmail.com>
2026-02-06 23:37:13 +0530

    resolved pipeline build error.

--- apps/shop/src/main.ts
+++ apps/shop/src/main.ts
@@ -1,5 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { appConfig } from './app/app.config.js';  // Add .js extension
+import { App } from './app/app';  // Add .js extension
 
-bootstrapApplication(App, appConfig).catch((err) => console.error(err));
+bootstrapApplication(App, appConfig).catch((err) =>
+  console.error(err)
+);
